c54997d4236aef0fab70e8b6ede797709c0dcaf1
Ricky Gian <rickygiankwan@gmail.com>
2021-09-02 03:31:36 +1000

    fixed up overflowing username in circle icon within the contact cards

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = ({ contact, clickMoreInfo }) => {
     return (
         < div className="contact" >
             <div style={{ color: 'white' }} className="contact__usernameCircle">
-                {contact.username}
+                {contact.username.length > 10 ? contact.username.substring(0, 10) : contact.username}
             </div>
             <div className="contact__summary">
                 <div className="contact__summary__name">
